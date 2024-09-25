3ad76c6eeda60042f262d113bf471dcce584d88b
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2024-09-25 20:24:22 +0200

    Fix PHPDoc

--- src/Interfaces/ConcatenableRelation.php
+++ src/Interfaces/ConcatenableRelation.php
@@ -7,7 +7,7 @@ interface ConcatenableRelation
     /**
      * Append the relation's through parents, foreign and local keys to a deep relationship.
      *
-     * @param \Illuminate\Database\Eloquent\Model[] $through
+     * @param string[] $through
      * @param array $foreignKeys
      * @param array $localKeys
      * @param int $position
